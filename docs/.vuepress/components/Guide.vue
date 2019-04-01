aeeb67313e577d0b8cf8143ea7f556c89a85fdf8
dengjy <dengjy@wangsu.com>
2019-04-01 16:50:39 +0800

    加了个动画

---
+++ docs/.vuepress/components/Guide.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="guide">
+    <div id="ui">
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+      <div class="text">母体科技</div>
+    </div>
+    <div class="position">
+      <div class="svg-wrapper">
+        <svg height="45" width="150" xmlns="http://www.w3.org/2000/svg">
+          <rect id="shape" height="40" width="150"></rect>
+        </svg>
+        <div id="post_bt">
+          <a href="data/img/ProgrammingLanguage.png" target="_blank">
+            <span class="spot"></span>下载图片
+          </a>
+        </div>
+      </div>
+      <!--Next button -->
+      <div class="svg-wrapper">
+        <svg height="45" width="150" xmlns="http://www.w3.org/2000/svg">
+          <rect id="shape" height="40" width="150"></rect>
+        </svg>
+        <div id="post_bt">
+          <a href="#" target="_blank">
+            <span class="spot"></span>下载Xmind
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    data() {
+      return {
+        ...this.$page.frontmatter
+      };
+    }
+  },
+  watch: {},
+  methods: {}
+};
+</script>
+
+<style lang="stylus" scoped>
+@import './styles/config.styl';
+@import './styles/magic.styl';
+@import './styles/guide.styl';
+</style>
